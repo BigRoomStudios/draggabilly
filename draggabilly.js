56147caafe1c0834feea08daeb09381a28a68cf3
WilliamSWoodruff <williamswoodruff@gmail.com>
2016-02-22 09:20:29 -0500

    cleanup

--- draggabilly.js
+++ draggabilly.js
@@ -347,20 +347,20 @@ proto.dragMove = function( event, pointer, moveVector ) {
   if ( !this.isEnabled ) {
     return;
   }
-  console.log(event);
+  
   var dragX = moveVector.x;
   var dragY = moveVector.y;
-  
+
   var grid = this.options.grid;
   var gridX = grid && grid[0];
   var gridY = grid && grid[1];
-  
+
   dragX = applyGrid( dragX, gridX );
   dragY = applyGrid( dragY, gridY );
-  
+
   dragX = this.containDrag( 'x', dragX, gridX );
   dragY = this.containDrag( 'y', dragY, gridY );
-  
+
   // constrain to axis
   dragX = this.options.axis == 'y' ? 0 : dragX;
   dragY = this.options.axis == 'x' ? 0 : dragY;
